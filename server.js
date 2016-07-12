8665ce08443a5c2b19475516963388683c62a044
thayer11 <holly.coddington@gmail.com>
2016-07-12 08:06:03 -0600

    Sprint3

--- server.js
+++ server.js
@@ -52,12 +52,12 @@ app.get('/api/albums', function album_index(req, res){
   });
 });
 
-// app.get('/api/albums/:id', function albumShow(req, res) {
-//   console.log('requested album id=', req.params.id);
-//   db.Album.findOne({_id: req.params.id}, function(err, album) {
-//     res.json(album);
-//   });
-// });
+app.get('/api/albums/:id', function albumShow(req, res) {
+  console.log('requested album id=', req.params.id);
+  db.Album.findOne({_id: req.params.id}, function(err, album) {
+    res.json(album);
+  });
+});
 
 
 app.post('/api/albums', function create(req, res){
@@ -74,21 +74,21 @@ db.Album.create(newAlbum, function(err, album){
     });
   });
 
-// app.post('/api/albums/:albumId/songs', function songsCreate(req, res) {
-//   console.log('body', req.body);
-//   db.Album.findOne({_id: req.params.albumId}, function(err, album) {
-//     if (err) { console.log('error', err); }
+app.post('/api/albums/:albumId/songs', function songsCreate(req, res) {
+  console.log('body', req.body);
+  db.Album.findOne({_id: req.params.albumId}, function(err, album) {
+    if (err) { console.log('error', err); }
 
-//     var song = new db.Song(req.body);
-//     album.songs.push(song);
-//     album.save(function(err, savedAlbum) {
-//       if (err) { console.log('error', err); }
-//       console.log('album with new song saved:', savedAlbum);
-//       res.json(song);
-//     });
-//   });
+    var song = new db.Song(req.body);
+    album.songs.push(song);
+    album.save(function(err, savedAlbum) {
+      if (err) { console.log('error', err); }
+      console.log('album with new song saved:', savedAlbum);
+      res.json(song);
+    });
+  });
 
-// });
+});
 
 /**********
  * SERVER *
